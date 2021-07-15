860501c5a686039d4929de822c1678d81dcd9359
Surafel Kifle <surafel@ienetworks.co>
2021-07-15 09:59:16 +0300

    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Homepages from './Pages/homepage/Homepage';
+function App() {
+  return (
+    <div>
+    <Homepages/>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/Components/menu-item/menu-item.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Menuitem=({title})=>(
+<div className="menu-item">
+    <div className="content">
+        <h1 className="title">HATS</h1>
+            <span className="subtitle"> SHOP NOW</span>
+    </div>
+</div>
+
+);
+
+export default Menuitem
